3c297306484c648c5eb58433d05d829a9511e55b
Abi Price <abi.price@btconnect.com>
2018-02-13 15:55:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(HELLO) {
+  return HELLO.toLowerCase()
+}
+
+
+function say(i love you grandma) {
+    return console.log("I can't hear you!")
+  }
+
+function say (I LOVE YOU GRANDMA){
+    return console.log("YES INDEED")
+  }
+
+function say("I love you Grandma") {
+    return console.log("I love you, too")
+}
